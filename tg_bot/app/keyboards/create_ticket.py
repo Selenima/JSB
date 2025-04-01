bb6984e4951b1fe705744630444482c888bf3b4a
luckyv1 <sinijsasa04@gmail.com>
2025-04-01 22:43:39 +0300

    dead

--- tg_bot/app/keyboards/create_ticket.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-def get_create_ticket_keyboard():
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(KeyboardButton("Добавить вложение"))
-    keyboard.add(KeyboardButton("Отправить заявку"))
--- tg_bot/app/keyboards/profile.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-def get_profile_keyboard():
-
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(KeyboardButton("Edit profile"))
-    return keyboard
