81f2f86434ea9a2bfed410ee14dd28de5443a1a5
luckyv1 <sinijsasa04@gmail.com>
2025-04-10 04:46:34 +0300

    jira worker composition

--- BACKEND/app/services/jira_service.py
+++ BACKEND/app/services/jira_service.py
@@ -1,24 +1,40 @@
 import asyncio
-from jira import JIRA
+from atlassian import Jira
 from typing import Optional
+
+from models.ticket import Ticket
+from schemas.ticket import TicketCreate, TicketResponse
+from utils.jira_issue import JiraIssue
+
 
 # jira_options = {'server': cfg.jira_server}
 # jira = JIRA(options=jira_options, basic_auth=(cfg.jira_user, cfg.jira_api))
 
 class JiraService:
-    def __init__(self, jira_client: JIRA, project_key: str = "SD", issue_type: str = "Task"):
+    def __init__(self, jira_client: Jira, project_key: str = "SD", issue_type: str = "Task"):
         self.jira = jira_client
         self.project_key = project_key
         self.issue_type = issue_type
 
-    async def create_issue(self, title: str, description: str) -> Optional[str]:
-        return await asyncio.to_thread(self._create_issue_sync, title, description)
+    async def create_issue(self, ticket: TicketCreate):
+        fields = self.create_data(ticket)
+        return await asyncio.to_thread(self._create_issue_sync, fields, ticket.tg_user_id)
 
-    def _create_issue_sync(self, title: str, description: str) -> str:
-        issue = self.jira.create_issue(
-            project={"key": self.project_key},
-            summary=title,
-            description=description,
-            issuetype={"name": self.issue_type}
+    def _create_issue_sync(self, fields: dict, tg_user_id: int) -> TicketResponse | False:
+        try:
+            issue = self.jira.create_issue(fields=fields)
+        except Exception as e:
+            return False #log
+        else:
+            issue = JiraIssue.from_dict(issue)
+            ticket = TicketResponse.model_validate(issue.work_data(tg_user_id))
+            return ticket
+
+    def create_data(self, ticket: TicketCreate) -> dict:
+        data = dict(
+            summary=ticket.title,
+            project=dict(key='SD'),
+            issuetype=dict(id=ticket.issue_type.service),
+            description=ticket.description
         )
-        return issue.key
+        return data
